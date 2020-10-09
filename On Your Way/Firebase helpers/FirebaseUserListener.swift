dc453ca3288ebd3faa8140cfc131c3d79b5695c7
Tariq Almazyad <Tariq.almazyad@hotmail.com>
2020-10-10 02:59:51 +0300

    first start

---
+++ On Your Way/Firebase helpers/FirebaseUserListener.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseUserListener.swift
+//  On Your Way
+//
+//  Created by Tariq Almazyad on 10/10/20.
+//
+
+import Foundation
+import Firebase
+
+
+class FirebaseUserListener {
+    
+    static let shared = FirebaseUserListener()
+    
+    private init() {}
+    
+}
---
+++ On Your Way/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  On Your Way
+//
+//  Created by Tariq Almazyad on 10/10/20.
+//
+
+import Foundation
